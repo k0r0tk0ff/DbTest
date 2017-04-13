1a1de1f1476391f987b664fd72260d728ef5de7a
k0r0tk0ff <korotkov34reg@gmail.com>
2017-04-13 22:45:12 +0300

    work with arraylist

--- src/main/java/k0r0tk0ff/exe/MainExecutor.java
+++ src/main/java/k0r0tk0ff/exe/MainExecutor.java
@@ -5,6 +5,8 @@ import k0r0tk0ff.entity.Articles;
 import k0r0tk0ff.entity.Types;
 import org.hibernate.Session;
 
+import java.util.ArrayList;
+
 /**
  * Created by k0r0tk0ff on 4/13/17.
  */
@@ -16,31 +18,29 @@ public class MainExecutor {
 
 		session.beginTransaction();
 
-		Types types = new Types();
+/*		Types types = new Types();
 		types.setName("Новости");
 
 		Articles articles = new Articles(types);
 		articles.setName("Новость1");
 
 		session.save(articles);
-		session.save(types);
+		session.save(types);*/
 
 
-		/*ArrayList <Types> typesArrayList = new ArrayList<>();
+		ArrayList<Types> typesArrayList = new ArrayList<>();
 		ArrayList <Articles> articlesArrayList = new ArrayList<>();
 
-		for (int i=1; i<4; i++) {
+		for (int i=0; i<4; i++) {
 			typesArrayList.add(i, new Types());
-			typesArrayList.get(i).setId(i);
 			typesArrayList.get(i).setName("Новости");
 
 			articlesArrayList.add(i, new Articles(typesArrayList.get(i)));
-			articlesArrayList.get(i).setId(i);
 			articlesArrayList.get(i).setName("Новость" + i);
 
 			session.save(typesArrayList.get(i));
 			session.save(articlesArrayList.get(i));
-		}*/
+		}
 
 		session.getTransaction().commit();
 		HibernateUtil.shutdown();
